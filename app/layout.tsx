4af14f428fbd71029fc628d35538daefdc96bfb3
Yellowsky <admin@ysky.app>
2023-05-31 00:30:26 +0200

    Added more favicons

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,12 @@
 import './globals.css'
 import {Inter} from 'next/font/google'
+import React from "react";
+import Favicon from "@/app/favicon";
 
 const inter = Inter({subsets: ['latin']})
 
 export const metadata = {
-    title: 'Yellowsky',
+    title: 'Yellow Sky',
     description: 'Next generation local networking',
 }
 
@@ -15,6 +17,10 @@ export default function RootLayout({
 }) {
     return (
         <html lang="en">
+        <head>
+            <title>Yellow Sky</title>
+            <Favicon/>
+        </head>
         <body className={inter.className}>{children}</body>
         </html>
     )
